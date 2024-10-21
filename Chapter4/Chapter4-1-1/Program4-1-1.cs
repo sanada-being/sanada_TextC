77eee9788139fa4ca79038e48a28f28ee2164e7b
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-10-21 13:23:45 +0900

    #517163   [C#テキスト]4章演習問題
    ・演習問題回答用のプロジェクトを作成しました。

---
+++ Chapter4/Chapter4-1-1/Program4-1-1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_1_1 {
+    internal class Program {
+        /* 問題4.1
+           以下の順にyearMonthクラスを定義してください。本章で学んだイディオムが使えるところは
+           イディオムを使ってください。
+
+           1. 年(Year)と月(Month)の2つのプロパティを持つクラスYearMonthを定義してください。
+              2つのプロパティは読み取り専用にし、値はコンストラクタで指定出来るようにしてください。
+              なお、引数で渡される月の値は1から12の範囲であるものと仮定してよいです。
+
+           2. YearMonthクラスに、Is21Centuryプロパティを追加してください。
+              2001年から2100年までが21世紀です。
+              この処理では加減乗除はしないでください。
+
+           3. YearMonthクラスに、1ヶ月を求めるAddOneMonthメソッドを追加してください。
+              このとき、自分自身のプロパティは変更せずに、新たなYearMonthオブジェクトを生成し、
+              その値を返してください。12月の時の処理に注意してください
+
+           4. ToStringメソッドをオーバーライドしてください。
+              結果は"2017年8月"といった形式にしてください。
+        */
+        static void Main(string[] args) {
+        }
+    }
+}
---
+++ Chapter4/Chapter4-1-2/Program4-1-2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_1_2 {
+    internal class Program {
+        /*
+        問題4.1で定義したYearMonthクラスを使って、次のコードを書いてください。
+        本章で学んだイディオムが使えるところは、イディオムを使ってください。
+
+        1. YearMonthを要素に持つ配列を定義し、初期値として5つのYearMonthを定義してください。
+
+        2. この配列の要素（YearMonthオブジェクトを全て列挙し、その値をコンソールに出力してください。
+
+        3. 配列の中の最初に見つかった21世紀のyearMonthオブジェクトを返すメソッドを書いてください。
+           見つからなかった場合は、nullを返してください。foreach文を使って実装してください。
+
+        4. 3で作成したメソッドを呼び出し、最初に見つかった21世紀のデータの年を表示してください。
+           見つからなければ、"21世紀のデータはありません"を表示してください。
+
+        5. 配列に格納されているすべてのYearMonthの1ヶ月後を求め、その結果を新たな配列に表示してください。
+           その後、その配列の要素の内容（年月）を順に表示してください。
+           LINQを使えるところはLINQを使って実装してください。
+        */
+        static void Main(string[] args) {
+        }
+    }
+}
