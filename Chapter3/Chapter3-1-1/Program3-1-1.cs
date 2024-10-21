f833f6f6f129cc46d76130d628a16b1054d5279b
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-10-21 11:16:45 +0900

    #517162  [C#テキスト]3章演習問題
    ・レビューの指摘内容を修正しました。
    ・RINQの変数sをコーディング規約に則り、xに変更しました。

--- Chapter3/Chapter3-1-1/Program3-1-1.cs
+++ Chapter3/Chapter3-1-1/Program3-1-1.cs
@@ -23,25 +23,22 @@ namespace Chapter3_1_1 {
 
             // 1.Exsitsメソッドを使い、8か9で割り切れる数があるかを調べる
             Console.WriteLine("問題1");
-            var wContainsMultiple8or9 = wNumbers.Exists(s => s % 8 == 0 || s % 9 == 0);
-            Console.WriteLine(wContainsMultiple8or9);
+            Console.WriteLine("8か9で割り切れる数があるかを判定　" + wNumbers.Exists(x => x % 8 == 0 || x % 9 == 0));
 
             // 2.ForEachメソッドを使い、各要素を2.0で割った値をコンソールに出力
             Console.WriteLine("問題2");
-            wNumbers.ForEach(s => Console.WriteLine(s / 2.0));
+            wNumbers.ForEach(x => Console.WriteLine(x / 2.0));
 
             // 3.LINQのWhereメソッドを使い、値が50以上の要素を列挙し、その結果をコンソールに出力
             Console.WriteLine("問題3");
-            var wNumbersOver50 = wNumbers.Where(s => s >= 50);
-            foreach (var wNumber in wNumbersOver50) {
+            foreach (var wNumber in wNumbers.Where(x => x >= 50)) {
                 Console.WriteLine(wNumber);
             }
 
             // 4.LINQのSelectメソッドを使い、それぞれの値を2倍し、その結果をList<int>に格納
             // その後、List<int>の各要素をコンソールに出力
             Console.WriteLine("問題4");
-            var wDoubleNumber = wNumbers.Select(s => s * 2).ToList();
-            foreach (var wNumber in wDoubleNumber) {
+            foreach (var wNumber in wNumbers.Select(x => x * 2).ToList()) {
                 Console.WriteLine(wNumber);
             }
         }
--- Chapter3/Chapter3-1-2/Program3-1-2.cs
+++ Chapter3/Chapter3-1-2/Program3-1-2.cs
@@ -29,32 +29,40 @@ namespace Chapter3_1_2 {
         "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","Hongkong",};
 
             // 1.FindIndexメソッドを使い、コンソールに入力した都市名が何番目に格納されているのかを出力
-            //   見つからなかったら-1を出力
+            //   見つからなかったら"その都市名は存在しません"と入力
             Console.WriteLine("問題1  都市名を入力");
             var wLine = Console.ReadLine();
-            int wIndex = wNames.FindIndex(s => s == wLine);
-            Console.WriteLine(wIndex);
+
+            if (wNames.FindIndex(x => x == wLine) >= 0) {
+                Console.WriteLine(wNames.FindIndex(x => x == wLine));
+            } else {
+                Console.WriteLine("その都市名は存在しません");
+            }
 
             // 2.LINQのCountメソッドを使い、小文字の'o'が含まれている都市名がいくつあるかカウントし、
             //   その結果をコンソールに出力
             Console.WriteLine("問題2");
-            var wCountO = wNames.Count(s => s.Contains('o'));
-            Console.WriteLine(wCountO);
+            Console.WriteLine(wNames.Count(x => x.Contains('o')));
 
             // 3.LINQのWhereメソッドを使い、小文字の'o'が含まれている都市名を抽出し、配列に格納してください。
             //   その後、配列の各要素をコンソールに出力してください。
             Console.WriteLine("問題3");
-            var wCitiesContainingO = wNames.Where(s => s.Contains("o"));
-            foreach (var w in wCitiesContainingO) {
-                Console.WriteLine(w);
+            foreach (var wContainOCities in wNames.Where(x => x.Contains("o"))) {
+                Console.WriteLine(wContainOCities);
             }
 
             // 4.LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数を抽出し、
             //   その文字数をコンソールに出力してください。都市名を表示する必要はありません。
             Console.WriteLine("問題4");
-            var wCitiesStartedB = wNames.Where(s => s[0] == 'B').Select(s => s.Length);
-            foreach (var t in wCitiesStartedB) {
-                Console.WriteLine(t);
+            foreach (var wStartedBCities in wNames.Where(x => x.StartsWith("B")).Select(x => x.Length)) {
+                Console.WriteLine(wStartedBCities);
+            }
+
+            // 追加問題
+            // 'n' で終わる都市名を抽出し、アルファベット順にならべたものを出力
+            Console.WriteLine("追加問題");
+            foreach (var wEndedTCities in wNames.Where(x => x.EndsWith("n")).OrderBy(x => x)) {
+                Console.WriteLine(wEndedTCities);
             }
         }
     }
