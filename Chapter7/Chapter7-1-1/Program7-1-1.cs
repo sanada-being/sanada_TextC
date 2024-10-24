5780efec0519191eef13f719dbcce00a7454cb68
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-10-24 17:09:46 +0900

    #517168 [C#テキスト]7章演習問題
    ・演習問題回答用のプロジェクトを作成しました。

---
+++ Chapter7/Chapter7-1-1/Program7-1-1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter7_1_1 {
+    internal class Program {
+        static void Main(string[] args) {
+            var wFlowerDict = new Dictionary<string, int>() {
+                ["aaa"] = 400,
+                ["bbb"] = 500,
+            };
+        }
+    }
+}
