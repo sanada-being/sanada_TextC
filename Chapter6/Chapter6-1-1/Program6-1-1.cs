0533110352293b6e316eaf4bcd13eeb4d0d85794
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-10-24 15:37:14 +0900

    #517166 [C#テキスト]6章演習問題
    ・演習問題の回答を作成しました。

---
+++ Chapter6/Chapter6-1-1/Program6-1-1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Chapter6_1_1 {
+    internal class Program {
+        /*
+        次のような配列が定義されています。
+        var wNumbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 }
+        この配列に対して、以下のコードを書いてください。
+
+        1. 最大値を求め、結果を表示してください。
+
+        2. 最後から2個目の要素を取り出してください。
+
+        3. それぞれの数値を文字列に変換し、結果を表示してください。
+
+        4. 数の小さい順に並べ、先頭から3つを取り出し、結果を表示してください。
+
+        5. 重複を排除した後、10より大きい値がいくつあるのかをカウントし、結果を表示してください。
+        */
+        static void Main(string[] args) {
+            // 1.
+            Console.WriteLine("問題1");
+            var wNumbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            Console.WriteLine(wNumbers.Where(x => x > 0).Max());
+
+            // 2.
+            Console.WriteLine("問題2");
+            Console.WriteLine(wNumbers.Skip(wNumbers.Length - 2).FirstOrDefault());
+
+            // 3.
+            Console.WriteLine("問題3");
+            var wStringNumbers = wNumbers.Select(x => x.ToString()).ToArray();
+            Console.WriteLine(string.Join(", ", wStringNumbers));
+
+            // 4.
+            Console.WriteLine("問題4");
+            var wThreeNumber = wNumbers.OrderBy(x => x).Where(y => y > 0).Take(3).ToArray();
+            Console.WriteLine(string.Join(",", wThreeNumber));
+
+            // 5.
+            Console.WriteLine("問題5");
+            var wBigerThanTen = wNumbers.Distinct().Count(x => x > 10);
+            Console.WriteLine(wBigerThanTen);
+        }
+    }
+}
---
+++ Chapter6/Program6-1.2/Program6-1-2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program6_1_2 {
+    internal class Program {
+        /*
+        次のようなリストが定義されています。
+             var wBooks = new List<Book> {
+               new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+               new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+               new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+               new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+               new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+               new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
+               new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+            };
+        このwBooksリストに対して、以下のコードを書いてください。
+
+        1. wBooksの中で、タイトルが"ワンダフル・C#ライフ"である書籍の価格とページ数を表示するコードを書いてください。
+
+        2. wBooksの中で、タイトルに"C#"が含まれている書籍が何冊あるかカウントするコードを書いてください。
+
+        3. wBooksの中で、タイトルに"C#"が含まれている書籍の平均ページ数を求めるコードを書いてください。
+
+        4. wBooksの中で、価格が4000円以上の本で最初に見つかった書籍のタイトルを表示するコードを書いてください。
+
+        5. wBooksの中で、価格が4000円未満の本の中で最大のページ数を求めるコードを書いてください。
+
+        6. wBooksの中で、ページが400ページ以上の書籍を、価格の高い順に表示(タイトルと価格を表示)するコードを書いてください。
+
+        7. wBooksの中で、タイトル"C#"が含まれていてかつ500ページ以下の本を見つけ、本のタイトルを表示するコードを書いてください。
+           複数見つかった場合は、そのすべてを表示してください。
+        */
+        static void Main(string[] args) {
+            var wBooks = new List<Book> {
+               new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+               new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+               new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+               new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+               new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+               new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
+               new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+            };
+
+            // 1.
+            Console.WriteLine("問題1");
+            var wFindTextBook = wBooks.FirstOrDefault(x => x.Title == "ワンダフル・C#ライフ");
+            if (wFindTextBook != null) {
+                Console.WriteLine($"{wFindTextBook.Price}円,{wFindTextBook.Pages}ページ");
+            } else {
+                Console.WriteLine("この書籍は見つかりません");
+            }
+
+            // 2.
+            Console.WriteLine("問題2");
+            Console.WriteLine(wBooks.Where(x => x.Title.Contains("C#")).Count());
+
+            // 3.
+            Console.WriteLine("問題3");
+            Console.WriteLine(wBooks.Where(x => x.Title.Contains("C#")).Average(x => x.Pages));
+
+            // 4.
+            Console.WriteLine("問題4");
+            var wBookFirstUnder4000Yen = wBooks.Where(x => x.Price >= 4000).FirstOrDefault();
+            if (wBookFirstUnder4000Yen != null) {
+                Console.WriteLine($"本のタイトルは「{wBookFirstUnder4000Yen.Title}」");
+            } else {
+                Console.WriteLine("4000円以下の本は見つかりません");
+            }
+
+            // 5.
+            Console.WriteLine("問題5");
+            Console.WriteLine(wBooks.Where(x => x.Price <= 4000).Max(x => x.Pages) + "ページ");
+
+            // 6. 
+            Console.WriteLine("問題6");
+            var w400PageOrMoreBook = wBooks.Where(x => x.Pages >= 400).OrderByDescending(x => x.Price).ToList();
+            w400PageOrMoreBook.ForEach(x => Console.WriteLine($"書籍名: {x.Title},　価格: {x.Price}円"));
+
+            // 7.
+            Console.WriteLine("問題7");
+            var w500pageOrLessCSharpBook = wBooks.Where(x => x.Title.Contains("C#") && x.Pages <= 500).ToList();
+            w500pageOrLessCSharpBook.ForEach(x => Console.WriteLine($"書籍名: {x.Title}"));
+        }
+    }
+}
