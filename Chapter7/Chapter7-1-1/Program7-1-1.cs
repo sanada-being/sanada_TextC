6b5933272dfc00b269c91e1d7472aea7fa82fbe8
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-10-25 17:21:05 +0900

    #517168　  [C#テキスト]7章演習問題
    ・演習問題１の回答を作成しました。

--- Chapter7/Chapter7-1-1/Program7-1-1.cs
+++ Chapter7/Chapter7-1-1/Program7-1-1.cs
@@ -1,16 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter7_1_1 {
     internal class Program {
+        /*
+        "Cozy lummox gives smart squid who asks for job pen"という文字列があります。
+        この文字列に対して、以下のコードを書いてください。
+
+        1. 各アルファベット文字（空白などアルファベット以外は除外)が何文字ずつ現れるかカウントするプログラムを書いてください。
+        この時に、必ずディクショナリクラスを使ってください。
+        大文字/小文字の区別はしないでください。
+        以下の形式で出力してください。
+        'A': 2
+        'B': 1
+        'C': 1  ....
+
+        2. 上記プログラムを、SortDictionary<TKey,TValue>を使って書き換えてください。
+        */
+
+
         static void Main(string[] args) {
-            var wFlowerDict = new Dictionary<string, int>() {
-                ["aaa"] = 400,
-                ["bbb"] = 500,
-            };
+            // 1.
+            string wSentnece = "Cozy lummox gives smart squid who asks for job pen";
+            var wLetterCounts = new SortedDictionary<char, int>();
+
+            foreach (char wCurrentChar in wSentnece.ToLower()) {
+                if (char.IsLetter(wCurrentChar)) {
+                    if (wLetterCounts.ContainsKey(wCurrentChar)) {
+                        wLetterCounts[wCurrentChar]++;
+                    } else {
+                        wLetterCounts[wCurrentChar] = 1;
+                    }
+                }
+            }
+
+            foreach(var wItem in wLetterCounts){
+                Console.WriteLine($"'{char.ToUpper(wItem.Key)}': {wItem.Value}");
+            }
         }
     }
 }
---
+++ Chapter7/Chapter7-1-2/Program7-1-2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter7_1_2 {
+    internal class Program {
+        /*
+        「7.3:ディクショナリえお使ったサンプルプログラム」で作成したプログラムに、以下の機能を追加してください。
+
+        1. ディクショナリに登録されている用語の数を返すCountプロパティをAbbreviationsクラスに追加してください。
+
+        2. 省力語を引数に受け取るRemoveメソッドをAbbreviationsクラスに追加してください。
+           要素が見つからない場合はfalseを、削除できた場合はtureを返してください。
+
+        3. CountプロパティとRemoveメソッドを利用するコードを書いてください。
+
+        4. 3文字の省略語だけを取り出し、以下の形式でコンソールに出力するコードを書いてください。
+           必要ならAbbreviationsクラスに新たなメソッドを追加してください。
+
+           ILO=国際労働機関
+           IMF=国際通貨基金  ....
+        */
+        static void Main(string[] args) {
+        }
+    }
+}
