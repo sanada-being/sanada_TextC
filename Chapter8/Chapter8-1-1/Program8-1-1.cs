a86badd2253654157880bca3e5dcd58b522575e2
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-11-01 16:53:17 +0900

    #517170 [C#テキスト]8章演習問題
    ・演習問題回答用のプロジェクトを作成しました。

---
+++ Chapter8/Chapter8-1-1/Program8-1-1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8_1_1 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+
+            var wToday = DateTime.Today;
+
+            int wDayOfYear = wToday.DayOfYear;
+            Console.WriteLine(wDayOfYear);
+
+            int wDay = DateTime.DaysInMonth(wToday.Year, wToday.Month);
+            var wEndOfMonth = new DateTime(wToday.Year,wToday.Month,wDay);
+            Console.WriteLine(wEndOfMonth);
+
+        }
+    }
+}
+
