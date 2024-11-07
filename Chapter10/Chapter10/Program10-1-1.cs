e7b682e21e68aa9a9fb7ca6b29ac8de199ac8b01
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-11-07 14:47:39 +0900

    #517173 [C#テキスト]10章演習問題
    ・演習問題回答用のプロジェクトを作成しました。

---
+++ Chapter10/Chapter10/Program10-1-1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Chapter10_1_1 {
+    internal class Program {
+        static void Main(string[] args) {
+            var wText = "RegexクラスのMatchメソッドを使います";
+            var wMatches = Regex.Matches(wText, @"\p{IsKatakana}+");
+            foreach (Match wMatch in wMatches) {
+                Console.Write($"{wMatch.Index},{wMatch.Length},{wMatch.Value}");
+            }
+        }
+    }
+}
