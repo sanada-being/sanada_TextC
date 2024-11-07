a125467c89c00a29ed076a2b93844fba869e2de5
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-11-07 18:52:01 +0900

    ・問題２の回答案

--- Chapter10/Chapter10-1-2/Program10-1-2.cs
+++ Chapter10/Chapter10-1-2/Program10-1-2.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Chapter10_1_2 {
     class Program {
         // テキストファイルを読み込み、3文字以上の数字だけから成る部分文字列をすべて抜き出すコードを書いてください。
         static void Main(string[] args) {
+            var wFilePath = @"aaa.txt";
+            var wLines = File.ReadAllLines(wFilePath).ToString();
+            var w3OrMoreNumbers = Regex.Matches(wLines, @"ここに絞り込み条件");
+            foreach (Match wMatch in w3OrMoreNumbers) {
+                Console.WriteLine($"{wMatch.Value}");
+            }
         }
     }
 }
