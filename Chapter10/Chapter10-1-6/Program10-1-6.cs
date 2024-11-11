c8fb890d8971ff6d1196044ff8d161fa36325e17
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-11-11 09:23:24 +0900

    #517173　[C#テキスト]10章演習問題
    ファイル名の編集と演習問題6の回答を作成しました。

--- Chapter10/Chapter10-1-6/Program10-1-6.cs
+++ Chapter10/Chapter10-1-6/Program10-1-6.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Chapter10_1_6 {
     class Program {
         //  5文字の回文とマッチする正規表現を考えてください。記号や数字だけから成る回文は除外してください。
         static void Main(string[] args) {
-            //検討中
             var wText = "しんぶんし ええいああ 11111 あいういあ level label とまと pop 111 ___ _@_ 1_1 @a@ あ4い4あ　12321 12_21 _____ 1_@_1 ";
-            var wPattern = @"\b(?!(?:[\d\W]{5})\b)(\w)(\w)\w\2\1\b";
-            var wAAA = @"\b(?!(?:[\d\W]{3})\b)(\w)\w\1\b";
-            foreach (Match wMatch in Regex.Matches(wText, wPattern)) {
+            
+            foreach (Match wMatch in Regex.Matches(wText, @"\b(?!(?:[\d\W]{5})\b)(\w)(\w)\w\2\1\b")) {
                 Console.WriteLine($"5文字の回文: {wMatch.Value}");
             }
         }
