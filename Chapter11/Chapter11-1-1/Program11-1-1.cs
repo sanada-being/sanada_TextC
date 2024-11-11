30d08d6baa6c3989c66478bf3aafee50e6a23864
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-11-11 15:33:41 +0900

    #517174　[C#テキスト]11章演習問題
    ・演習問題の回答を作成しました。

--- Chapter11/Chapter11-1-1/Program11-1-1.cs
+++ Chapter11/Chapter11-1-1/Program11-1-1.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Chapter11_1_1 {
-    internal class Program {
+    class Program {
+        /*
+       1. XMLファイルを読み込み、競技名とチームメンバー数の一覧を表示してください。
+
+       2. 最初にプレイされた年の若い順に漢字の競技名を出力してください。
+
+       3. メンバーがもっとも多い競技名を表示してください
+
+       4. サッカーの情報を追加して、新たなXMLファイルに出力してください。
+          ファイル名は特に指定しません。サッカーの情報は調べてください。
+       */
         static void Main(string[] args) {
+            var wFilePath = @"..\..\Sample.xml";
+
+            if (!File.Exists(wFilePath)) {
+                Console.WriteLine("このファイルは存在しません");
+                return;
+            }
+
+            // 1.
+            Console.WriteLine("問題1");
+            var wHmlFile = XDocument.Load(wFilePath);
+            var wSportsElements = wHmlFile.Root.Elements();
+            foreach (var wXElement in wSportsElements) {
+                var wName = wXElement.Element("name");
+                var wMenberNum = wXElement.Element("teammembers");
+                Console.WriteLine($"競技名:{wName.Value}, チームメンバー:{wMenberNum.Value}人");
+            }
+
+            // 2. 
+            Console.WriteLine("問題2");
+            var wFirstPlaySports = wSportsElements.OrderByDescending(x => (int)(x.Element("firstplayed")));
+            foreach (var wSortedSportElement in wFirstPlaySports) {
+                var wName = wSortedSportElement.Element("name");
+                var wKanaName = wName.Attribute("kanji");
+                Console.WriteLine($"競技名:{wKanaName?.Value}");
+            }
+
+            // 3.
+            Console.WriteLine("問題3");
+            var wMostManyPlayer = wSportsElements.OrderByDescending(x => (int)x.Element("teammembers")).First();
+            var wSportsName = wMostManyPlayer.Element("name")?.Value;
+            Console.WriteLine($"最もチームメンバーが多いスポーツ:{wSportsName}");
+
+            // 4.
+            Console.WriteLine("問題4");
+            var wNewSportsElement = new XElement("ballsport",
+                new XElement("name", new XAttribute("kanji", "蹴球"), "サッカー"),
+                new XElement("teammembers", 11),
+                new XElement("firstplayed", 1863)
+                );
+            wHmlFile.Root.Add(wNewSportsElement);
+            wHmlFile.Save(@"..\..\NewHmlFile");
+            Console.WriteLine("新しい情報の追加が完了しました。");
         }
     }
 }
--- Chapter11/Chapter11-1-2/Program11-1-2.cs
+++ Chapter11/Chapter11-1-2/Program11-1-2.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Chapter11_1_2 {
-    internal class Program {
+    class Program {
+        /*
+        XMLファイル"Sample11-2.xml"を以下の形式に変換し、別のファイルに保存してください。
+
+          <?xml version="1.0" encoding="utf-8"?>
+          <difficultkanji>
+            <word kanji="鬼灯" yomi="ほおずき" />
+            <word kanji="暖簾" yomi="のれん" />
+            <word kanji="杜撰" yomi="ずさん" />
+            <word kanji="坩堝" yomi="るつぼ" />
+          </difficultkanji>
+        */
         static void Main(string[] args) {
+            var wFilePath = @"..\..\Sample11-2.xml";
+            if (!File.Exists(wFilePath)) {
+                Console.WriteLine("このファイルは存在しません");
+            }
+
+            var wDoc = XDocument.Load(wFilePath);
+            var wWordDictionary = wDoc.Descendants("word").ToDictionary(x => x.Element("kanji")?.Value, x => x.Element("yomi")?.Value);
+            var wNewDoc = new XElement("difficultkanji", wWordDictionary.Select(x => new XElement("word", new XAttribute("kanji", x.Key), new XAttribute("yomi", x.Value))));
+
+            wNewDoc.Save(@"..\..\NewFile");
+            Console.WriteLine("新しいファイルが作成されました");
         }
     }
 }
