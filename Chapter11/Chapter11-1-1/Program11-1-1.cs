54506700f959d37a956b69c4cce235020eebd33b
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-11-14 14:17:25 +0900

    #517174 [C#テキスト]11章演習問題
    ・レビューの指摘内容を修正しました。

--- Chapter11/Chapter11-1-1/Program11-1-1.cs
+++ Chapter11/Chapter11-1-1/Program11-1-1.cs
@@ -6,7 +6,7 @@ using System.Xml.Linq;
 namespace Chapter11_1_1 {
     class Program {
         /*
-       1. XMLファイルを読み込み、競技名とチームメンバー数の一覧を表示してください。
+       1. XMLファイル@"..\..\Sample.xml";を読み込み、競技名とチームメンバー数の一覧を表示してください。
 
        2. 最初にプレイされた年の若い順に漢字の競技名を出力してください。
 
@@ -16,7 +16,8 @@ namespace Chapter11_1_1 {
           ファイル名は特に指定しません。サッカーの情報は調べてください。
        */
         static void Main(string[] args) {
-            var wFilePath = @"..\..\Sample.xml";
+            Console.WriteLine("ファイルパスを入力してください");
+            var wFilePath = Console.ReadLine();
 
             if (!File.Exists(wFilePath)) {
                 Console.WriteLine("このファイルは存在しません");
@@ -25,28 +26,39 @@ namespace Chapter11_1_1 {
 
             // 1.
             Console.WriteLine("問題1");
-            var wHmlFile = XDocument.Load(wFilePath);
-            var wSportsElements = wHmlFile.Root.Elements();
-            foreach (var wXElement in wSportsElements) {
-                var wName = wXElement.Element("name");
-                var wMenberNum = wXElement.Element("teammembers");
-                Console.WriteLine($"競技名:{wName.Value}, チームメンバー:{wMenberNum.Value}人");
+            var wXmlFile = XDocument.Load(wFilePath);
+            var wSportsElements = wXmlFile.Root.Elements();
+            foreach (var wSport in wSportsElements) {
+                var wName = wSport.Element("name");
+                var wPlayerCount = wSport.Element("teammembers");
+                Console.WriteLine($"競技名:{wName.Value}, チームメンバー:{wPlayerCount.Value}人");
             }
 
             // 2. 
             Console.WriteLine("問題2");
-            var wFirstPlaySports = wSportsElements.OrderByDescending(x => (int)(x.Element("firstplayed")));
-            foreach (var wSortedSportElement in wFirstPlaySports) {
-                var wName = wSortedSportElement.Element("name");
-                var wKanaName = wName.Attribute("kanji");
-                Console.WriteLine($"競技名:{wKanaName?.Value}");
+            foreach (var wSortedSportsElement in wSportsElements.OrderBy(x => (int)x.Element("firstplayed"))) {
+                var wName = wSortedSportsElement.Element("name");
+                Console.WriteLine($"競技名:{wName.Attribute("kanji")?.Value ?? "なし"}");
+
             }
 
             // 3.
             Console.WriteLine("問題3");
-            var wMostManyPlayer = wSportsElements.OrderByDescending(x => (int)x.Element("teammembers")).First();
+            var wMostManyPlayer = wSportsElements.OrderByDescending(x => {
+                int wTeamMembers;
+                return int.TryParse(x.Element("teammembers")?.Value, out wTeamMembers) ? wTeamMembers : 0;
+            }).First();
+
             var wSportsName = wMostManyPlayer.Element("name")?.Value;
             Console.WriteLine($"最もチームメンバーが多いスポーツ:{wSportsName}");
+
+            var wTeamMembersValue = wMostManyPlayer.Element("teammembers")?.Value;
+            int wParsedMembers;
+            if (int.TryParse(wTeamMembersValue, out wParsedMembers)) {
+                Console.WriteLine($"チームメンバー数: {wParsedMembers}");
+            } else {
+                Console.WriteLine("チームメンバー数が無効な値です");
+            }
 
             // 4.
             Console.WriteLine("問題4");
@@ -55,8 +67,8 @@ namespace Chapter11_1_1 {
                 new XElement("teammembers", 11),
                 new XElement("firstplayed", 1863)
                 );
-            wHmlFile.Root.Add(wNewSportsElement);
-            wHmlFile.Save(@"..\..\NewHmlFile");
+            wXmlFile.Root.Add(wNewSportsElement);
+            wXmlFile.Save(@"..\..\NewXMLFile");
             Console.WriteLine("新しい情報の追加が完了しました。");
         }
     }
--- Chapter11/Chapter11-1-2/Program11-1-2.cs
+++ Chapter11/Chapter11-1-2/Program11-1-2.cs
@@ -6,7 +6,7 @@ using System.Xml.Linq;
 namespace Chapter11_1_2 {
     class Program {
         /*
-        XMLファイル"Sample11-2.xml"を以下の形式に変換し、別のファイルに保存してください。
+        XMLファイル"..\..\Sample11-2.xml"を以下の形式に変換し、別のファイルに保存してください。
 
           <?xml version="1.0" encoding="utf-8"?>
           <difficultkanji>
@@ -17,16 +17,21 @@ namespace Chapter11_1_2 {
           </difficultkanji>
         */
         static void Main(string[] args) {
-            var wFilePath = @"..\..\Sample11-2.xml";
+            Console.WriteLine("ファイルパスを入力してください");
+            var wFilePath = Console.ReadLine();
+
             if (!File.Exists(wFilePath)) {
                 Console.WriteLine("このファイルは存在しません");
+                return;
             }
 
             var wDoc = XDocument.Load(wFilePath);
-            var wWordDictionary = wDoc.Descendants("word").ToDictionary(x => x.Element("kanji")?.Value, x => x.Element("yomi")?.Value);
-            var wNewDoc = new XElement("difficultkanji", wWordDictionary.Select(x => new XElement("word", new XAttribute("kanji", x.Key), new XAttribute("yomi", x.Value))));
+            var wWords = wDoc.Descendants("word");
 
-            wNewDoc.Save(@"..\..\NewFile");
+            var wNewDoc = new XElement("difficultkanji", wWords.Select
+                (x => new XElement("word", new XAttribute("kanji", x.Element("kanji")?.Value), new XAttribute("yomi", x.Element("yomi")?.Value))));
+
+            wNewDoc.Save(@"..\..\NewFile.xml");
             Console.WriteLine("新しいファイルが作成されました");
         }
     }
