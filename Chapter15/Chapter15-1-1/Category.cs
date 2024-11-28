765306667656042c4473bba50ba7aa4ebe15fd21
眞田　アクバルシャー(SANADA akubarusha) <sanada@beingcorp.co.jp>
2024-11-28 11:03:30 +0900

    Revert "#517187   [C#テキスト]15章演習問題"
    This reverts commit f88893047834d87efe8d439391c15e13efb78657.

--- Chapter15/Chapter15-1-1/Category.cs
+++ Chapter15/Chapter15-1-1/Category.cs
@@ -1,4 +1,10 @@
-﻿namespace Chapter15_1_1 {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter15_1_1 {
     /// <summary>
     ///カテゴリクラス
     /// </summary>
