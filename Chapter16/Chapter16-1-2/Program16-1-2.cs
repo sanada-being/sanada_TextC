ed3402b70e892196e7ad357eacaf3b1969ceb0e9
sanada <sanada@beingcorp.co.jp>
2024-12-02 17:49:43 +0900

    #517183　[C#テキスト] 16章演習問題
    ・演習問題の解答を半分作成しました。

---
+++ Chapter16/Chapter16-1-2/Program16-1-2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter16_1_2 {
+    /*
+    指定したディレクトリにあるC#のソースファイルの中をすべて検索し、キーワードasyncとawaitの両方を利用しているファイルを列挙してください。
+    列挙する際はファイルのフルパスを表示してください。表示する順番は問いません。
+    並列処理をした場合としない場合の2つのバージョンを作成し、どれくらい速度に差があるかも調べてください。
+    */
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
